f2b3958b0f9cfe4e1bd1f0269f27b09cd8e1444f
Sharath Babu Rayapalyam Chandrashekar <cwc5421@tu-harburg.de>
2020-11-02 19:18:00 +0100

    lists work

--- grpc-example-2/Assets/Scripts/IcpClient.cs
+++ grpc-example-2/Assets/Scripts/IcpClient.cs
@@ -1,8 +1,11 @@
 ﻿using Grpc.Core;
 using Protoicp;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class IcpClient {
+    // public static List<string> items = new List<string>();
     
     private readonly CalcTransform.CalcTransformClient _client;
     private readonly Channel _channel;
@@ -71,16 +74,29 @@ public class IcpClient {
             Z = 2.0f
         };
         
+        Google.Protobuf.Collections.RepeatedField<Point> pts = new Google.Protobuf.Collections.RepeatedField<Point>();
+        pts.Add(pt1);
+        pts.Add(pt2);
+
         Cloud cloud1 = new Cloud
         {
-            Points = { pt1, pt2, pt3, pt4 }
+            Points = {}
         };
+
+        cloud1.Points.Add(pt1);
+        cloud1.Points.Add(pt2);
+        cloud1.Points.Add(pt3);
+        cloud1.Points.Add(pt4);
 
         Cloud cloud2 = new Cloud
         {
             Points = { pt5, pt6, pt7, pt8 }
         };
         
+        Cloud cloud3 = new Cloud
+        {
+            // Points = {}
+        };
 
         ObjectClouds objClouds = new ObjectClouds
         {
